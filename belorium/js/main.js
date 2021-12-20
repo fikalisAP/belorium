1b18512c53a3383827ac73f21aa9392bc47cdb29
BOB <93865766+fikalisAP@users.noreply.github.com>
2021-12-20 11:21:24 +0500

    Add files via upload

---
+++ belorium/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+	$('.header__slider-inner').slick({
+		// autoplay: true,
+		// autoplaySpeed: 2000,
+		// arrows: false,
+		draggable: true,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		arrows: false,
+		dots: false,
+		fade: true,
+		speed: 2500,
+		infinite: true,
+		cssEase: 'ease-in-out',
+		touchThreshold: 100
+	});
+})
