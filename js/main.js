c10a3399cf7c20ea2d850780a268d812483b08e3
BOB <ivan.sherbetov@yandex.ru>
2021-12-22 10:33:16 +0500

    adventage slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+	var $window = $(window);
+	var windowsize = $window.width();
 	$('.header__slider-inner').slick({
 		// autoplay: true,
 		// autoplaySpeed: 2000,
@@ -14,28 +16,45 @@ $(function () {
 		cssEase: 'ease-in-out',
 		touchThreshold: 100
 	});
+	if (windowsize <= 900) {
+		$('.adventage-inner').slick({
+			dots: true,
+			arrows: false,
+			slidesToShow: 1,
+			slideToScroll: 1,
+			mobileFirst: true,
+			responsive: [{
+				breakpoint: 650,
+				settings: {
+					slidesToShow: 2,
+					slideToScroll: 2,
+				},
+			}]
+		});
+	}
+
 })
 
-$('.item__color *').click(function(){
-    if($(this).hasClass('active')){
-        $(this).removeClass('active')
+$('.item__color *').click(function () {
+	if ($(this).hasClass('active')) {
+		$(this).removeClass('active')
 		$(this).css('border', 'none')
-    } else {
-        $(this).addClass('active')
+	} else {
+		$(this).addClass('active')
 		$(this).css('border', '3px solid #443C3A')
-    }
+	}
 });
 
 $(function () {
-    $('.another__tittle__wrapper').click(function (event) {
-        $(this).toggleClass('active').children().next().slideToggle(300);
+	$('.another__tittle__wrapper').click(function (event) {
+		$(this).toggleClass('active').children().next().slideToggle(300);
 		if ($(this).hasClass('active')) {
 			$('.another__tittle__wrapper.active').children().children('img').attr("src", "../images/downArrow.svg");
 		}
 		else {
 			$('.another__tittle__img').attr("src", "../images/upArrow.svg");
 		}
-    });
+	});
 });
 
 $(function () {
@@ -47,11 +66,12 @@ $(function () {
 			$(this).toggleClass('footer__item-title__active');
 		}
 	});
-	$('.menu__burger').on('click', function(){
-        $('.menu').slideToggle();
+	$('.menu__burger').on('click', function () {
+		$('.menu').slideToggle();
 		$('.container').css('padding', '0px')
-    })
+	})
 });
-$('.catalog__item').click(function(){
+
+$('.catalog__item').click(function () {
 	window.location.href = '../pages/good.html';
 });
